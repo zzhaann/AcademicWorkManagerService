637223edfe3b3284bbb28ec4cb7d640db6d46cb6
zzhaann <zhhaann@mail.ru>
2025-06-03 15:40:42 +0500

    Domain User class

---
+++ AcademicWorkManagerService.Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicWorkManagerService.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+        public string UserRole { get; set; }    
+    }
+}
---
+++ AcademicWorkManagerService.Domain/Enums/UserRole.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicWorkManagerService.Domain.Enums
+{
+    public enum UserRole
+    {
+        STUDENT,
+        SUPERVISOR,
+        DEPARTMENT,
+        MANAGER,
+        EXPERTS,
+        VICERECTOR,
+        ADMIN
+    }
+}
