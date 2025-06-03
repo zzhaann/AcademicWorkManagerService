62ced8810fbc89eee1885064adae23e17f77bcb3
BaUka0 <bauirzhan.bisanov@gmail.com>
2025-06-03 16:29:36 +0500

    настроена связь с бд ef core Npgsql

---
+++ AcademicWorkManagerService.Infrastructure/Services/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AcademicWorkManagerService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicWorkManagerService.Infrastructure.Services
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> users { get; set; }
+    }
+}
--- AcademicWorkManagerServicePresentation/Program.cs
+++ AcademicWorkManagerServicePresentation/Program.cs
@@ -1,8 +1,18 @@
-using AcademicWorkManagerService.Application.Interfaces;
+using Npgsql;
+using AcademicWorkManagerService.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+#region db
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+#endregion
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
